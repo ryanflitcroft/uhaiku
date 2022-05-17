755879a3fb23261ec163a391117d24c0e2733195
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-17 15:52:37 -0700

    created PrivateRoute

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
-export default function PrivateRoute() {
-  return <div>PrivateRoute</div>;
+export default function PrivateRoute({ children, ...rest }) {
+  let auth = useAuth();
+
+  return (
+    <Route 
+      {...rest}
+      render={({ location }) =>
+        auth.user ? (
+          children
+        ) : (
+          <Redirect 
+            to={{
+              pathname: '/auth',
+              state: { origin: location },
+            }}
+          />
+        )
+      }
+    />
+  );
 }
